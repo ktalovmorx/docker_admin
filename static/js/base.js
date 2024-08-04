047b3b59f0ce232f1e5b4b5f4e238c564daf491e
Jose Ernesto Morales Ventura <jomorales.ventura@gmail.com>
2024-08-04 19:01:08 -0400

    Aplicar confirmacion de accion en javascript

--- static/js/base.js
+++ static/js/base.js
@@ -1,86 +1,93 @@
 function run_instance(name){
-    alert(`Running ${name}...`);
-
-    const data = {'name':name};
-    fetch('/run_instance',{
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      })
-      .then(response => response.json())
-      .then(data => {
-          //document.getElementById('report_win_btn').disabled = true;
-          //document.getElementById('report_loss_btn').disabled = true;
-          Swal.fire({
-            title: 'Recibido',
-            text: data.message,
-            icon: "info",
-            confirmButtonText: "OK"
-          });
-          /*Actualizar monto a colocar para que administrador sepa lo que ocurre*/
-          console.log(data);
-      }).catch(error => {
-        // Manejar cualquier error que ocurra
-        console.error(error);
-      });
+    let c = confirm('Seguro que desea iniciar la instancia');
+    if (c){
+      alert(`Running ${name}...`);
+      const data = {'name':name};
+      fetch('/run_instance',{
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => {
+            //document.getElementById('report_win_btn').disabled = true;
+            //document.getElementById('report_loss_btn').disabled = true;
+            Swal.fire({
+              title: 'Recibido',
+              text: data.message,
+              icon: "info",
+              confirmButtonText: "OK"
+            });
+            /*Actualizar monto a colocar para que administrador sepa lo que ocurre*/
+            console.log(data);
+        }).catch(error => {
+          // Manejar cualquier error que ocurra
+          console.error(error);
+        });
+    } 
 }
 
 function stop_instance(name){
-    alert(`Stopping ${name}...`);
-
-    const data = {'name': name};
-    fetch('/stop_instance',{
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      })
-      .then(response => response.json())
-      .then(data => {
-          //document.getElementById('report_win_btn').disabled = true;
-          //document.getElementById('report_loss_btn').disabled = true;
-          Swal.fire({
-            title: 'Recibido',
-            text: data.message,
-            icon: "info",
-            confirmButtonText: "OK"
-          });
-          /*Actualizar monto a colocar para que administrador sepa lo que ocurre*/
-          console.log(data);
-      }).catch(error => {
-        // Manejar cualquier error que ocurra
-        console.error(error);
-      });
+    let c = confirm('Seguro que desea iniciar la instancia');
+    if (c){
+      alert(`Stopping ${name}...`);
+      const data = {'name': name};
+      fetch('/stop_instance',{
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => {
+            //document.getElementById('report_win_btn').disabled = true;
+            //document.getElementById('report_loss_btn').disabled = true;
+            Swal.fire({
+              title: 'Recibido',
+              text: data.message,
+              icon: "info",
+              confirmButtonText: "OK"
+            });
+            /*Actualizar monto a colocar para que administrador sepa lo que ocurre*/
+            console.log(data);
+        }).catch(error => {
+          // Manejar cualquier error que ocurra
+          console.error(error);
+        });
+    }
 }
 
 function restart_instance(name){
   alert(`Restarting ${name}...`);
+  if (c){
+    alert(`Stopping ${name}...`);
+    const data = {'name': name};
+    fetch('/restart_instance',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then(response => response.json())
+      .then(data => {
+          //document.getElementById('report_win_btn').disabled = true;
+          //document.getElementById('report_loss_btn').disabled = true;
+          Swal.fire({
+            title: 'Recibido',
+            text: data.message,
+            icon: "info",
+            confirmButtonText: "OK"
+          });
+          /*Actualizar monto a colocar para que administrador sepa lo que ocurre*/
+          console.log(data);
+      }).catch(error => {
+        // Manejar cualquier error que ocurra
+        console.error(error);
+      });
+  }
 
-  const data = {'name': name};
-  fetch('/restart_instance',{
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(data => {
-        //document.getElementById('report_win_btn').disabled = true;
-        //document.getElementById('report_loss_btn').disabled = true;
-        Swal.fire({
-          title: 'Recibido',
-          text: data.message,
-          icon: "info",
-          confirmButtonText: "OK"
-        });
-        /*Actualizar monto a colocar para que administrador sepa lo que ocurre*/
-        console.log(data);
-    }).catch(error => {
-      // Manejar cualquier error que ocurra
-      console.error(error);
-    });
 }
