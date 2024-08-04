e04c2f7ef8d84da7ec0d32f19f71325ed3c37cb0
Jose Ernesto Morales Ventura <jomorales.ventura@gmail.com>
2024-08-04 17:46:16 -0400

    Agregar index.html

--- static/js/base.js
+++ static/js/base.js
@@ -55,3 +55,32 @@ function stop_instance(instance_name){
         console.error(error);
       });
 }
+
+function restart_instance(instance_name){
+  alert(`Restarting ${instance_name}...`);
+
+  const data = {'instance_name': instance_name};
+  fetch('/restart_instance',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        //document.getElementById('report_win_btn').disabled = true;
+        //document.getElementById('report_loss_btn').disabled = true;
+        /*Swal.fire({
+          title: 'Recibido',
+          text: data.message,
+          icon: "info",
+          confirmButtonText: "OK"
+        });*/
+        /*Actualizar monto a colocar para que administrador sepa lo que ocurre*/
+        console.log(data);
+    }).catch(error => {
+      // Manejar cualquier error que ocurra
+      console.error(error);
+    });
+}
